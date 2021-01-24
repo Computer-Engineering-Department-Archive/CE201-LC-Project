722e0cd6429019fdbefb84d08adc58b1e61d041b
BARDIA <bardia.ardakanian@yahoo.com>
2021-01-24 10:24:57 +0330

    sel for mode added

--- codes/phase 1/part 1/32 bit add-subtract/adder-subtractor-32bit/adderSubtractor.v
+++ codes/phase 1/part 1/32 bit add-subtract/adder-subtractor-32bit/adderSubtractor.v
@@ -21,16 +21,17 @@
 module adderSubtractor(
 		input [31:0] A,
 		input [31:0] B,
+		input sel,
 		output [31:0] S,
 		output co
     );
 	 
 	 wire [32:0] cin;
-	 assign cin[0] = 0;
+	 assign cin[0] = sel;
 	 genvar i;
 	 for ( i = 0; i < 32; i = i + 1 )
 	 begin
-			FA fa(A[i], B[i], cin[i], S[i], cin[i+1]);
+			FA fa(A[i], B[i]^sel, cin[i], S[i], cin[i+1]);
 	 end
 	 
 	 assign co = cin[32];
