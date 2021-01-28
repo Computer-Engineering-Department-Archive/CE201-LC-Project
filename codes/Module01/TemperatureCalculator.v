efbbb9a66afd18576a9a9b525683f475f0109f63
BARDIA <bardia.ardakanian@yahoo.com>
2021-01-28 15:56:23 +0330

    module 2 testbench added

--- codes/Module01/TemperatureCalculator.v
+++ codes/Module01/TemperatureCalculator.v
@@ -27,21 +27,19 @@ module TemperatureCalculator (
 	output [31:0] tempc      // temp [temperature celsius]
 );
 
-	/* write your code here */
 	wire [7:0] tempRef;
 	assign tempRef=~(tc_ref-1);
 	wire [31:0] tempbase;
 	assign tempbase=~(tc_base-1);
 	wire [15:0] out;
-	Multiplier8x8 multi(.A(tempRef[7:0]),.B(tempRef[7:0]),.P(out[15:0]));
+	Multiplier8x8 m8x8(.A(tempRef[7:0]),.B(tempRef[7:0]),.P(out[15:0]));
 	wire [31:0] out1;
-	Multiplier16x16 multi1(.A(out[15:0]),.B({1'b0,adc_data[14:0]}),.P(out1[31:0]));
+	Multiplier16x16 m16x16(.A(out[15:0]),.B({1'b0,adc_data[14:0]}),.P(out1[31:0]));
 	wire  [31:0] out2;
-	AdderSubtractor32x32 adder(.A(tempbase[31:0]),.B(out1[31:0]),.sel(adc_data[15]),.S(out2[31:0]));
 	wire [31:0] ttempc;
-	division devide(.A(out2[31:0]),.B({32'b00000000000000000000000000100000}),.Res(ttempc[31:0]));
-	assign tempc=~ttempc+1;
+	division div(.A(out1[31:0]),.B({32'b00000000000000000000000000100000}),.Res(out2[31:0]));
+	AdderSubtractor32x32 adder32x32(.A(tempbase[31:0]),.B(out2[31:0]),.sel(adc_data[15]),.S(ttempc[31:0]));
+		assign tempc=~ttempc+1;
 	
-	/* write your code here */
 
 endmodule
--- codes/Module02/coDetector.v
+++ codes/Module02/coDetector.v
@@ -22,12 +22,12 @@ module coDetector(x,Z,CLK,RST);
    input x,CLK,RST;
    output Z;
 	
-   reg [3:0] state;
+   reg [3:0] state = 4'b0000;
    parameter S0 = 4'b0000, S1 = 4'b0001, S2 = 4'b0010, S3 = 4'b0011, S4 = 4'b0100, S5 = 4'b0101, S6 = 4'b0110, S7 = 4'b0111, S8 = 4'b1000, S9 = 4'b1001,S10 = 4'b1010,S11 = 4'b1011,S12 = 4'b1100;
       always @ (posedge CLK or negedge RST)
          if (~RST) state = S0;  //Initialize to state S0
 	    else
-case (state)
+			case (state)
             S0: if (~x)  state = S0; else state = S1; 
             S1: if (~x)  state = S2; else state = S1;   
             S2: if (~x)  state = S0; else state = S3;
@@ -41,8 +41,6 @@ case (state)
 				S10: if (~x)  state = S0; else state = S11; 
 			   S11: if (~x)  state = S0; else state = S12; 
 				S12: if (~x)  state = S2; else state = S1; 
-				
          endcase
-		assign
-			Z=state[3]&state[2];
+		assign Z=state[3]&state[2];
 endmodule
