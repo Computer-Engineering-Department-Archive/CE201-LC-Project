63da1a90507d392a9d4476ee3780497e909780cf
BARDIA <bardia.ardakanian@yahoo.com>
2021-02-07 12:16:05 +0330

    Module06 testbench

--- codes/Module06/MemoryUnitUtils.v
+++ codes/Module06/MemoryUnitUtils.v
@@ -1,25 +1,66 @@
-/*--  *******************************************************
---  Computer Architecture Course, Laboratory Sources 
---  Amirkabir University of Technology (Tehran Polytechnic)
---  Department of Computer Engineering (CE-AUT)
---  https://ce[dot]aut[dot]ac[dot]ir
---  *******************************************************
---  All Rights reserved (C) 2019-2020
---  *******************************************************
---  Student ID  : 
---  Student Name: 
---  Student Mail: 
---  *******************************************************
---  Additional Comments:
---
---*/
+`timescale 1ns / 1ps
 
-/*-----------------------------------------------------------
----  Module Name:  Memory Unit Utils
----  Description: Module5:
------------------------------------------------------------*/
-`timescale 1 ns/1 ns
-module MemoryUnitUtils();
-	
-	
-end module
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   12:07:53 02/07/2021
+// Design Name:   MemoryUnit
+// Module Name:   E:/git/LCF99/codes/Module06/MemoryUnitUtils.v
+// Project Name:  Module06
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: MemoryUnit
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module MemoryUnitUtils;
+
+	// Inputs
+	reg arst;
+	reg clk;
+	reg wren;
+	reg [34:0] din;
+
+	// Outputs
+	wire [34:0] dout;
+
+	// Instantiate the Unit Under Test (UUT)
+	MemoryUnit uut (
+		.arst(arst), 
+		.clk(clk), 
+		.wren(wren), 
+		.din(din), 
+		.dout(dout)
+	);
+
+	initial begin
+		// Initialize Inputs
+		arst = 1;
+		clk = 0;
+		wren = 0;
+		din = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+		arst = 0;
+		wren = 1;
+      din = 35'b1010010011100110011110100010011;
+		// Add stimulus here
+
+
+	 forever begin
+		#10 clk = ~clk;
+	 end
+	end
+      
+endmodule
+
